76a0d0f59c79fce1abe77f372312a16c72533326
Alexander Koskovich <akoskovich@pm.me>
2024-09-03 16:53:16 -0400

    caimito: Build complete partition set and add proprietary files
    Change-Id: I84210e77057d574a9c3a7674fc10dc2944bbba35

---
+++ komodo/extract-files.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=komodo
+VENDOR=google
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../../.."
+
+export TARGET_ENABLE_CHECKELF=true
+
+# If XML files don't have comments before the XML header, use this flag
+# Can still be used with broken XML files by using blob_fixup
+export TARGET_DISABLE_XML_FIXING=true
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+ONLY_FIRMWARE=
+KANG=
+SECTION=
+CARRIER_SKIP_FILES=()
+VENDOR_SKIP_FILES=()
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        --only-firmware)
+            ONLY_FIRMWARE=true
+            ;;
+        -n | --no-cleanup)
+            CLEAN_VENDOR=false
+            ;;
+        -k | --kang)
+            KANG="--kang"
+            ;;
+        -s | --section)
+            SECTION="${2}"
+            shift
+            CLEAN_VENDOR=false
+            ;;
+        *)
+            SRC="${1}"
+            ;;
+    esac
+    shift
+done
+
+if [ -z "${SRC}" ]; then
+    SRC="adb"
+fi
+
+function prepare_firmware() {
+    if [ "${SRC}" != "adb" ]; then
+        bash "${ANDROID_ROOT}"/lineage/scripts/pixel/prepare-firmware.sh "${DEVICE}" "${SRC}"
+    fi
+}
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+
+if [ -z "${ONLY_FIRMWARE}" ]; then
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+    generate_prop_list_from_image "product.img" "${MY_DIR}/proprietary-files-carriersettings.txt" CARRIER_SKIP_FILES carriersettings
+    extract "${MY_DIR}/proprietary-files-carriersettings.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+    readarray -t VENDOR_SKIP_FILES < <(cat "${MY_DIR}/skip-files-vendor.txt" | sed -E "/^[[:blank:]]*(#|$)/d")
+    VENDOR_TXT="${MY_DIR}/proprietary-files-vendor.txt"
+    generate_prop_list_from_image "vendor.img" "${VENDOR_TXT}" VENDOR_SKIP_FILES
+
+    set_module "vendor/lib64/com.google.edgetpu_app_service-V4-ndk.so" "com.google.edgetpu_app_service-V4-ndk-vendor" "${VENDOR_TXT}"
+    set_module "vendor/lib64/com.google.edgetpu_vendor_service-V2-ndk.so" "com.google.edgetpu_vendor_service-V2-ndk-vendor" "${VENDOR_TXT}"
+    set_module "vendor/lib64/vendor.google.whitechapel.audio.audioext@4.0.so" "vendor.google.whitechapel.audio.audioext@4.0-vendor" "${VENDOR_TXT}"
+    set_module "vendor/lib64/vendor.google.whitechapel.audio.extension-V2-ndk.so" "vendor.google.whitechapel.audio.extension-V2-ndk-vendor" "${VENDOR_TXT}"
+    set_module "vendor/lib64/vendor.samsung_slsi.telephony.hardware.oemservice@1.0.so" "vendor.samsung_slsi.telephony.hardware.oemservice@1.0-vendor" "${VENDOR_TXT}"
+    set_module "vendor/lib64/vendor.samsung_slsi.telephony.hardware.radioExternal@1.0.so" "vendor.samsung_slsi.telephony.hardware.radioExternal@1.0-vendor" "${VENDOR_TXT}"
+
+    extract "${MY_DIR}/proprietary-files-vendor.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+fi
+
+if [ -z "${SECTION}" ]; then
+    extract_firmware "${MY_DIR}/proprietary-firmware.txt" "${SRC}"
+fi
+
+"${MY_DIR}/setup-makefiles.sh"
