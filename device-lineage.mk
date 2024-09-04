c3e749070e701b6b9dc68338869c8e63a481a86a
Alexander Koskovich <akoskovich@pm.me>
2024-09-04 18:37:26 -0400

    caimito: Add release flags for zumapro
    These are required to enable the AIDL HALs for audio & battery.
    
    Change-Id: Iec7c49dfd29b8451958aad7cf91bc45d32a561bd

--- device-lineage.mk
+++ device-lineage.mk
@@ -4,5 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Audio
+RELEASE_PIXEL_AIDL_AUDIO_HAL := true
+
+# Battery
+RELEASE_PIXEL_AIDL_BATTERY_MITIGATION_HAL := true
+
 # Wireless Charging
 include device/google/gs-common/wireless_charger/wireless_charger.mk
