f223b0e9e80242360cdfd6681c6b455160c7c98d
Alexander Koskovich <akoskovich@pm.me>
2024-09-05 04:04:47 -0400

    caimito: Add overlays from AD1A.240530.047
    Change-Id: I533319a8d61cc67f9111f133f49cdc727eb27ece

--- device-lineage.mk
+++ device-lineage.mk
@@ -10,5 +10,24 @@ RELEASE_PIXEL_AIDL_AUDIO_HAL := true
 # Battery
 RELEASE_PIXEL_AIDL_BATTERY_MITIGATION_HAL := true
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += device/google/caimito/overlay-lineage/vendor
+PRODUCT_PACKAGE_OVERLAYS += device/google/caimito/overlay-lineage/product
+
+ifneq (,$(filter lineage_caiman, $(TARGET_PRODUCT)))
+PRODUCT_PACKAGES += \
+    PixelDisplayServiceOverlayCaiman
+endif
+
+ifneq (,$(filter lineage_komodo, $(TARGET_PRODUCT)))
+PRODUCT_PACKAGES += \
+    PixelDisplayServiceOverlayKomodo
+endif
+
+ifneq (,$(filter lineage_tokay, $(TARGET_PRODUCT)))
+PRODUCT_PACKAGES += \
+    PixelDisplayServiceOverlayTokay
+endif
+
 # Wireless Charging
 include device/google/gs-common/wireless_charger/wireless_charger.mk
