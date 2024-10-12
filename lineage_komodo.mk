2a96baa5b313cd752380e20aa48e14b2ab611c13
slogar25 <michaelslogar@gmail.com>
2024-10-12 14:31:39 +0000

    Changes for los bring up

--- lineage_komodo.mk
+++ lineage_komodo.mk
@@ -11,22 +11,22 @@ include device/google/caimito/device-lineage.mk
 
 # Flags
 TARGET_BOOT_ANIMATION_RES := 1440
-TARGET_SUPPORTS_BLUR := true
-TARGET_HAS_UDFPS := true
-EXTRA_UDFPS_ICONS := true
-EXTRA_UDFPS_ANIMATIONS := true
+#TARGET_SUPPORTS_BLUR := true
+#TARGET_HAS_UDFPS := true
+#EXTRA_UDFPS_ICONS := true
+#EXTRA_UDFPS_ANIMATIONS := true
 
 # Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
+#TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Launcher fucking madness
 #TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
-TARGET_DEFAULT_PIXEL_LAUNCHER := true
-TARGET_INCLUDE_PIXEL_LAUNCHER := true
+#TARGET_DEFAULT_PIXEL_LAUNCHER := true
+#TARGET_INCLUDE_PIXEL_LAUNCHER := true
 
 # Lame ass flags
-WITH_GAPPS := true
-WITH_GMS := true
+#WITH_GAPPS := true
+#WITH_GMS := true
 
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 9 Pro XL
@@ -43,8 +43,8 @@ PRODUCT_NAME_FOR_ATTESTATION := komodo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=komodo \
-    PRIVATE_BUILD_DESC="komodo-user 14 AD1A.240530.047 12143574 release-keys"
+    PRIVATE_BUILD_DESC="komodo-user 14 AD1A.240905.004 12196292 release-keys"
 
-BUILD_FINGERPRINT := google/komodo/komodo:14/AD1A.240530.047/12143574:user/release-keys
+BUILD_FINGERPRINT := google/komodo/komodo:14/AD1A.240905.004/12196292:user/release-keys
 
 $(call inherit-product, vendor/google/komodo/komodo-vendor.mk)
