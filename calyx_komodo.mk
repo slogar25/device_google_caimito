13560b48e4855d014bf115673b162795c00bf7d0
Michael Bestas <mkbestas@gmail.com>
2024-10-17 18:45:43 +0300

    Switch to new property override scheme
    Change-Id: I0c302a8cc38a1171991a83860bdcdaf6e26dc9dd

--- calyx_komodo.mk
+++ calyx_komodo.mk
@@ -21,13 +21,10 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 9 Pro XL
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
-PRODUCT_SYSTEM_NAME := $(DEVICE_CODENAME)
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME) \
-    PRIVATE_BUILD_DESC="komodo-user 15 AP3A.241005.015 12366759 release-keys"
-
-BUILD_FINGERPRINT := google/komodo/komodo:15/AP3A.241005.015/12366759:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES := \
+    BuildDesc=komodo-user[[:space:]]15[[:space:]]AP3A.241005.015[[:space:]]12366759[[:space:]]release-keys \
+    BuildFingerprint=google/komodo/komodo:15/AP3A.241005.015/12366759:user/release-keys \
+    DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
